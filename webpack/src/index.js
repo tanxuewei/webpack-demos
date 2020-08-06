175175bb333a2e96b91a1c549e4f53144f8119a0
tanxuewei <hztanxuewei@corp.netease.com>
2020-08-06 17:45:42 +0800

    添加tree shaking

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,12 @@
 import add from './add.js'
 import { minus } from './minus.js'
-// import { chunk } from 'lodash-es'
+import { chunk } from 'lodash-es'
+import { a } from './util'
 
 const sum = add(1, 2)
 const division = minus(2, 1)
 
 console.log(sum)
 console.log(division)
-// console.log(chunk(['a', 'b', 'c'], 2))
+console.log(chunk(['a', 'b', 'c'], 2))
+console.log(a())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -9,15 +9,29 @@ module.exports = {
     chunkFilename: '[name].js'
   },
   optimization: {
+    usedExports: true,
     splitChunks: {
       chunks: 'all',
       cacheGroups: {
+        lodash: {
+          name: 'lodash', // 单独将 lodash 拆包
+          priority: 10, // 优先级要大于 commons 不然会被打包进 commons
+          test: /[\\/]node_modules[\\/]lodash-es[\\/]/
+        },
         vendors: {
-          name: 'vendors'
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
         }
       }
     }
   },
-  mode: 'development',
+  // mode: 'development',
+  mode: 'production',
   devtool: 'source-map'
 };
